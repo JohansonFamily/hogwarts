6b80d055b202ed3ce8c3f983dd7a122ecf993953
matt <matt@johansonfamily.com>
2021-11-19 06:09:10 -0600

    vcs

--- hb.py
+++ hb.py
@@ -6,6 +6,7 @@ root.title('Hogwarts Battle')
 root.geometry("1500x1000+100+100")
 #root.config(bg='white')
 
+
 def escape(e):
 	root.quit()
 
@@ -15,8 +16,8 @@ root.bind("<Escape>", escape)
 root.option_add("*Font", "Arial 14")
 
 # Setup initial root dimensions to span width of window
-root.grid_rowconfigure(0,weight=1)
-root.grid_columnconfigure(0,weight=1)
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(0, weight=1)
 
 # Create a new game instance and start it.  Eventually, we can add loading a saved game or selecting the game here.
 game.createGame(root)
