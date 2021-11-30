f51aa11b499eb3658eff475d2736e95a097c60d2
matt <matt@johansonfamily.com>
2021-11-29 21:03:51 -0600

    refactored ui to start to build out gameboard

--- ui.py
+++ ui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkmacosx import Button
 from PIL import ImageTk, Image
+import game
 
 class Imgbutton:
 
@@ -13,45 +14,53 @@ class Imgbutton:
         self.command = command
 
         img = self.canvas.create_image(self.x, self.y, image=self.image, anchor='nw')
-        mainCanvas.tag_bind(img, "<1>", self.command)
+        self.canvas.tag_bind(img, "<1>", self.command)
 
-        mainCanvas.bind("<Button-1>", on_click)
-        mainCanvas.bind("<B1-Motion>", on_drag)
+        self.canvas.bind("<Button-1>", self.on_click)
+        self.canvas.bind("<B1-Motion>", self.on_drag)
 
-    def click(self):
-        selected = mainCanvas.find_overlapping(event.x - 0, event.y - 0, event.x + 0, event.y + 0)
+    def on_click(self, e):
+        selected = self.canvas.find_overlapping(event.x - 0, event.y - 0, event.x + 0, event.y + 0)
         if selected[-1] != 1:
-            mainCanvas.selected = selected[-1]  # select the top-most item
-            mainCanvas.startxy = (event.x, event.y)
+            self.canvas.selected = selected[-1]  # select the top-most item
+            self.canvas.startxy = (event.x, event.y)
             # print(mainCanvas.selected, mainCanvas.startxy)
         else:
-            mainCanvas.selected = None
+            self.canvas.selected = None
 
-    def on_drag(event):
-        if mainCanvas.selected:
+    def on_drag(event, e):
+        if self.canvas.selected:
             # calculate distance moved from last position
-            dx, dy = event.x - mainCanvas.startxy[0], event.y - mainCanvas.startxy[1]
+            dx, dy = event.x - self.canvas.startxy[0], event.y - self.canvas.startxy[1]
             # move the selected item
-            mainCanvas.move(mainCanvas.selected, dx, dy)
+            self.canvas.move(self.canvas.selected, dx, dy)
             # update last position
-            mainCanvas.startxy = (event.x, event.y)
+            self.canvas.startxy = (event.x, event.y)
 
 
-def myfun(e):
-    root.quit()
+class GameBoard:
+
+    def __init__(self, root):
+        self.root = root
+
+        imgRaw = Image.open('images/bg.jpg')
+        imgResized = imgRaw.resize((1500, 1000), Image.ANTIALIAS)
+        self.imgProcessed = ImageTk.PhotoImage(imgResized)
+
+        self.mainCanvas = tk.Canvas(self.root, height=1000, width=1500
+                                    , bd=0, highlightthickness=0, relief='ridge')
+        self.mainCanvas.place(x=0, y=0, anchor='nw')
+        self.mainCanvas.create_image(0, 0, image=self.imgProcessed, anchor='nw')
+
+
 
 root = tk.Tk()
 root.title('Hogwarts Battle')
 root.geometry("1500x1000+100+100")
-#root.wm_attributes('-transparentcolor','black')
+# removes header
+#root.overrideredirect(0)
 
-imgRaw = Image.open('images/bg.jpg')
-imgResized = imgRaw.resize((1500, 1000), Image.ANTIALIAS)
-imgProcessed = ImageTk.PhotoImage(imgResized)
-
-aimgRaw = Image.open('images/coin.png')
-aimgResized = aimgRaw.resize((50, 40), Image.ANTIALIAS)
-aimgProcessed = ImageTk.PhotoImage(aimgResized)
+# root.config(bg='white')
 
 # This function, tied to the escape key, will end the entire program
 def escape(e):
@@ -62,34 +71,17 @@ def escape(e):
 
 # This ties the escape key to ending the program
 root.bind("<Escape>", escape)
+
+# Add fonts for all the widgets
+root.option_add("*Font", "Arial 14")
+
+# Setup initial root dimensions to span width of window
 root.grid_rowconfigure(0, weight=1)
-root.grid_rowconfigure(1, weight=1)
+root.grid_columnconfigure(0, weight=1)
 
-topframe = tk.Frame(root, width=1500, height=1000)
-topframe.pack(fill='both', expand=True)
-#tk.Label(topframe, text="Hello World!").grid(row=0)
+# Create a new game instance and start it.  Eventually, we can add loading a saved game or selecting the game here.
+# game.selectGame(root)
 
-# Display the image on a label
-label = tk.Label(topframe, image=aimgProcessed, pady=50, bg='#eeefff')
-# Set the label background color to a transparent color
-label.config(bg='#eeefff')
-label.pack()
+gb = GameBoard(root)
 
-bottomframe = tk.Frame(root, width=1500, height=500, bg='systemTransparent')
-bottomframe.pack()
-
-mainCanvas=tk.Canvas(bottomframe, height=1000, width=1500)
-mainCanvas.place(x=0, y=0, anchor='nw')
-btn = tk.Button(bottomframe, image=aimgProcessed, command=root.quit, bd=0, bg='#eeefff')
-mainCanvas.create_image(0,0,image=imgProcessed, anchor='nw')
-mainCanvas.create_image(200,100,image=aimgProcessed, anchor='nw')
-mainCanvas.create_text(200, 200, text="Some text", fill='white')
-mainCanvas.create_window(200,300, window=btn)
-img = mainCanvas.create_image(200,100,image=aimgProcessed, anchor='nw')
-mainCanvas.tag_bind(img, "<1>", myfun)
-coin = Imgbutton(mainCanvas, 200, 400, aimgProcessed)
-
-# img.config(bg='systemTransparent')
-
-
-root.mainloop()
+tk.mainloop()
